e7e1f09b4c7f6b193c86bdb653085c18140a98b8
Li Zhineng <im@zhineng.li>
2025-07-01 10:13:45 +0800

    format

--- server.mjs
+++ server.mjs
@@ -4,15 +4,15 @@ const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
 
   if (req.method === 'GET' && req.url === '/gettime') {
-      res.end(JSON.stringify({
-        time: Math.floor(Date.now() / 1000)
-      }))
+    res.end(JSON.stringify({
+      time: Math.floor(Date.now() / 1000)
+    }))
   } else if (req.method === 'GET' && req.url.startsWith('/eagle')) {
-      res.end(JSON.stringify({
-          status: 200, data: { eagleId: 0 }
-      }))
+    res.end(JSON.stringify({
+      status: 200, data: { eagleId: 0 }
+    }))
   } else {
-      res.end('It works!\n')
+    res.end('It works!\n')
   }
 })
 
